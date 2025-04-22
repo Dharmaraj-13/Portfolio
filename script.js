f33422f6d24df43529aaab6376a142cebfd6c104
Dharmaraj A <dharmaraj01313@gmail.com>
2025-04-22 14:11:36 +0530

    Update script.js

--- script.js
+++ script.js
@@ -6,12 +6,13 @@ const close = document.querySelector('.close-icon');
 
 
 menu.addEventListener("click",function(){
-  sideBar.classList.add("open-sidebar");
   sideBar.classList.remove("close-sidebar");
+  sideBar.classList.add("open-sidebar");  
 })
 close.addEventListener("click",function(){
-  sideBar.classList.add("close-sidebar");
   sideBar.classList.remove("open-sidebar");
+  sideBar.classList.add("close-sidebar");
+  
 })
 
 
